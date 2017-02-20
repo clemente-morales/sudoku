dd90ee48ab9b6ef1d2b2acee5bc7c37c7e8003b7
Clemente Morales Fernandez <morales.fernandez.clemente@gmail.com>
2017-02-19 19:29:11 -0500

    Sudoku feature Add funtion to Eliminate values from peers of each box with a single value

--- solution.py
+++ solution.py
@@ -77,7 +77,26 @@ def display(values):
     return
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    solution = sudoku.copy()
+    for k, v in sudoku.items():
+        if len(v) == 1 :
+            peer_keys = peers[k]
+
+            for peer_key in peer_keys:
+                solution[peer_key] = "".join(filter(lambda char: char != v, solution[peer_key]))
+    
+    
+    return solution 
 
 def only_choice(values):
     pass
